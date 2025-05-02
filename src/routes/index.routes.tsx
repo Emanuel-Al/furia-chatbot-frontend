f87800c00ea4052d39bfaadbcaa42408ac51fa99
Emanuel Reino <emanuelreino13@gmail.com>
2025-05-02 17:08:07 -0300

    feat: chat route protected

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -3,6 +3,7 @@ import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
 import Chat from "../pages/Chat/Chat";
 import Home from "../pages/Home/Home";
+import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute";
 const RouterProvider = () => {
   return (
     <BrowserRouter>
@@ -10,7 +11,14 @@ const RouterProvider = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/chat" element={<Chat />} />
+        <Route
+          path="/chat"
+          element={
+            <ProtectedRoute>
+              <Chat />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
