b20ae4761f2c6e0d06e0f8e31d39c5219aa38ce9
Emanuel Reino <emanuelreino13@gmail.com>
2025-05-01 23:45:42 -0300

    feat: chatInput component

---
+++ src/components/ChatInput/ChatInput.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { TextField } from "@mui/material";
+import { IoMdSend } from "react-icons/io";
+
+const ChatInput = () => {
+  return <TextField variant="outlined" color="primary" />;
+};
+
+export default ChatInput;
